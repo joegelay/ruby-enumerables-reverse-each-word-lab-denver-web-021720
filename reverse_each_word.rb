8d6b4a84ac289bc70d6954af57b3e8270636107a
Joe Gelay <joegelay@gmail.com>
2020-01-29 19:55:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
-  array = str.split()
+  
   result = []
-  array.each{|word| result.push(word.reverse)}
+  str.split().each{|word| result.push(word.reverse)}
   reversed_string = result.join(" ")
   return reversed_string
 end
