3ac3a6e955f6905b59142031d82d479c97bf53c2
Joe Gelay <joegelay@gmail.com>
2020-01-29 19:56:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@
 
 def reverse_each_word(str)
   result = []
-  str.split().each{|word| result.push(word.reverse)}
+  str.split().collect{|word| result.push(word.reverse)}
   reversed_string = result.join(" ")
   return reversed_string
 end
