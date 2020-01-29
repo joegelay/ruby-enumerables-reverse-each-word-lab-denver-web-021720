9ce9c0cf32a7ee05bea64f1faf2f60e2e0e8bc7c
Joe Gelay <joegelay@gmail.com>
2020-01-29 19:55:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
+# def reverse_each_word(str)
+#   result = []
+#   str.split().each{|word| result.push(word.reverse)}
+#   reversed_string = result.join(" ")
+#   return reversed_string
+# end
+
 def reverse_each_word(str)
   result = []
   str.split().each{|word| result.push(word.reverse)}
